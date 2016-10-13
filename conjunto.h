1ee28dd846dd09b2e71cddb93e198f82db086842
Eduardo Sutil <sutil.edu@gmail.com>
2016-10-13 08:49:50 -0300

    Início do algoritmo follow

---
+++ conjunto.h
@@ -0,0 +1,18 @@
+//
+// Created by sutil on 13/10/16.
+//
+
+#ifndef PREDICTIONTABLE_CONJUNTO_H
+#define PREDICTIONTABLE_CONJUNTO_H
+
+#include <stdio.h>
+
+struct conjunto
+{
+    char *valores;
+    char key;
+};
+
+typedef struct conjunto Conjunto;
+
+#endif //PREDICTIONTABLE_CONJUNTO_H
---
+++ follow.h
@@ -0,0 +1,13 @@
+//
+// Created by sutil on 13/10/16.
+//
+
+#ifndef PREDICTIONTABLE_FOLLOW_H
+#define PREDICTIONTABLE_FOLLOW_H
+
+#include "conjunto.h"
+#include "readgramar.h"
+
+Conjunto* follow(GramaticalRule* gramaticalRules);
+
+#endif //PREDICTIONTABLE_FOLLOW_H
