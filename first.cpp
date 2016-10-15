ffab835a7dcde050749d5dd92f1cfa0ca73a382c
Lucas <martiolilucas@gmail.com>
2016-10-15 14:32:36 -0300

    Iniciando implementação algoritmo First

--- first.cpp
+++ first.cpp
@@ -6,15 +6,30 @@
 
 FirstSet *first(GramaticalRule *gramaticalrule)
 {
+    FirstSet *head = (FirstSet*) malloc(sizeof(FirstSet));
     int mudou = 0;
+
     do
     {
-        GramaticalRule currentrule = gramaticalrule;
+        GramaticalRule *currentrule = gramaticalrule;
         while (currentrule != NULL)
         {
             char key = currentrule->key;
+            FirstSet *currentfirstset = seekkey(head,key);
+            if (currentfirstset == NUlL)
+            {
+                currentfirstset = (FirstSet*) malloc(sizeof(FirstSet));
+                currentfirstset->key = key;
+                currentfirstset->set = (char*) malloc(500);
+            }
 
+            for (int i = 0; i < strlen(currentrule->rule); i++)
+            {
+                char crule = currentrule->rule[i];
+                if (islower(crule))
+                    addfirst();
 
+            }
 
             currentrule = currentrule->next;
         }
