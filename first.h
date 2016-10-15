ede91d1ed3877de95af7ea3af898584070aff66e
Lucas <martiolilucas@gmail.com>
2016-10-15 14:00:57 -0300

    First

--- first.h
+++ first.h
@@ -5,4 +5,17 @@
 #ifndef PREDICTIONTABLE_FIRST_H
 #define PREDICTIONTABLE_FIRST_H
 
+    #include "readgramar.h"
+
+    struct firstset
+    {
+        struct firstset *next;
+        char *set;
+        char key;
+    };
+
+    typedef struct firstset FirstSet;
+
+    FirstSet *first(GramaticalRule *gramaticalrule)
+
 #endif //PREDICTIONTABLE_FIRST_H
