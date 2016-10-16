a82fb29954efe34b7a6e00840c4408b211de9dd4
echotech01 <dev1@echomobile.com.br>
2016-10-16 20:36:42 -0200

    Começando o gerador de Tabela

---
+++ geraTabela.c
@@ -0,0 +1,33 @@
+//
+// Created by Victor on 16/10/2016.
+//
+
+#include <stdio.h>
+#include "readgrammar.h"
+#include "first.c"
+#include "follow.c"
+
+
+
+void geraPredTable(){
+
+
+    for (int i = 0; i < ; ++i) {
+        for (int j = 0; j < ; ++j) {
+
+            case 1:{
+
+            }
+
+            case 2:{
+
+            }
+
+            case 3:{
+
+            }
+        }
+        
+    }
+
+}
