9f1d649223c0aaffdf42b3a5e3288f9f3b9fec6f
Lucas <martiolilucas@gmail.com>
2016-10-15 23:31:50 -0300

    Escreve o conjunto First em arquivo.

--- main.c
+++ main.c
@@ -3,8 +3,12 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+#include <malloc.h>
 #include "readgramar.h"
 #include "first.h"
+
+int writefirstset(FirstSet *firstset);
 
 int main(int argc, char *argv[])
 {
@@ -30,13 +34,46 @@ int main(int argc, char *argv[])
     }
 
     FirstSet * f = first(s);
-    while (f != NULL)
-    {
-        printf("Key: %c Set: %s\n", f->key, f->set);
-        f = f->next;
-    }
+    if (!writefirstset(f))
+        return 0;
 
 
     return 1;
+}
+
+int writefirstset(FirstSet *firstset)
+{
+    FILE *firstfile = fopen("C:\\Users\\Lucas\\first.txt", "w");
+
+    if (firstfile == NULL)
+    {
+        printf("Problemas na criação do arquivo first\n");
+        return 0;
+    }
+
+    while (firstset != NULL)
+    {
+        char *desc1 = "Conjunto: ";
+        char *notterminal = (char*) malloc(2);
+        notterminal[0] = firstset->key;
+        notterminal[1] = '\0';
+        char *desc2 = " = {";
+
+        if(fwrite(desc1, 1, strlen(desc1), firstfile) != strlen(desc1) ||
+           fwrite(notterminal, 1, 1, firstfile)       != 1             ||
+           fwrite(desc2, 1, strlen(desc2), firstfile) != strlen(desc2) ||
+           fwrite(firstset->set, 1, strlen(firstset->set), firstfile) != strlen(firstset->set) ||
+           fwrite("}\n", 1, 2, firstfile) != 2)
+        {
+            printf("Erro na escrita do arquivo");
+            return 0;
+        }
+        firstset = firstset->next;
+    }
+
+    fclose(firstfile);
+    return 1;
+
+
 }
 
